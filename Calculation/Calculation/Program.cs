c9b1b4666c7b2ec4ab1a74359614694c8fe18d1b
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-08-30 12:47:50 +0630

    Initial Commit

---
+++ Calculation/Calculation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = x + 7;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
