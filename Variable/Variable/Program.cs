c669eb8655e65ad485ef6f292e8fa4bfd674d66a
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-08-31 10:50:01 +0630

    Test Variable Sum

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num1;
+            int num2;
+            double num3;
+
+            num1 = 10;
+            num2 = 20;
+            num3 = num1 + num2;
+            Console.WriteLine("First Number = {0}, Second Number = {1}, Sum = {2}", num1, num2, num3);
+            Console.ReadLine();
+        }
+    }
+}
