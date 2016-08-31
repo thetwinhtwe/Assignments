140b78871b5dec2beaa0d2f0d4ca79a478f0e62b
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-08-31 12:58:33 +0630

    Test Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[100]; 
+
+            for (int i = 0; i < 100; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            foreach (int j in n)
+            {
+                int i = j - 100;
+                Console.WriteLine("Element[{0}] = {1}", i, j);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
