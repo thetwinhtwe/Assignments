ec01c44832416759ad47b2873928504e7ebf06ff
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-08-31 17:26:11 +0630

    Test Array App

---
+++ Array2/Array2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            string[,] names = new string[5, 4];
+
+            byte[][] scores = new byte[5][];
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = new byte[i + 3];
+            }
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+            }
+            Console.ReadLine();
+        }
+    }
+}
