ee5efcdfcf77338e044622ae3a9d14709efda277
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-08-31 11:11:34 +0630

    Write Biggest Number Application

---
+++ BiggestNum/BiggestNum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggestNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("\nEnter First Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter Second Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine("\nFirst Number is bigger than the Second");
+            else
+                Console.WriteLine("\nSecond Number is bigger than the First");
+
+            Console.ReadLine();
+        }
+    }
+}
