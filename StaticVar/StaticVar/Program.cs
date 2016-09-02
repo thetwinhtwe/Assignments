51e2d309e74329678d1a334f51ef211cdaeeaece
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 12:24:54 +0630

    Test Static Variable App

---
+++ StaticVar/StaticVar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVar
+{
+    class Program
+    {
+        class StaticVar
+        {
+            public static int num=0;
+            public void count()
+            {
+                num++;
+            }
+            public static int getNum()
+            {
+                return num;
+            }
+        }
+        class StaticTester
+        {
+            static void Main(string[] args)
+            {
+                StaticVar s = new StaticVar();
+                s.count();
+                s.count();
+                s.count();
+                Console.WriteLine("Variable num: {0}", StaticVar.getNum());
+                Console.ReadKey();
+            }
+        }
+    }
+}
