72062b62cbc62dbe558d84d9d5f9b37ffaa2be56
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 12:50:22 +0630

    Test Polymorphism

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    
+        {
+      void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+
+            p.print(5);
+
+
+            p.print(500.263);
+
+
+            p.print("Hello C++");
+            Console.ReadKey();
+        }
+    }
+}
+
