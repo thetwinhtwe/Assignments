c6e372219b81faddcccf5e2ba9b08c628daabc2b
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 14:18:29 +0630

    Write Fruits Array Console App

---
+++ FruitArray/FruitArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apple", "banana", "orange" };
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine("Fruits: {0}", fruits[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
