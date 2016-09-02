8e7f3be6868f4f760fef36f5d90b74f99a013b17
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 14:45:19 +0630

    Test Lists Console App

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(7);
+
+
+            foreach (int prime in list)
+            {
+                System.Console.WriteLine(prime);
+                Console.ReadLine();
+            }
+        }
+    }
+}
