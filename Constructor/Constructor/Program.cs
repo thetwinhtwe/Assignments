a407d41455e1bed699536f9cf4b7cb811b637bba
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 11:00:46 +0630

    Test Constructor Class

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        private double length;   
+        public Program()
+        {
+            Console.WriteLine("Object is being created");
+        }
+
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public double getLength()
+        {
+            return length;
+        }
+
+        static void Main(string[] args)
+        {
+            Program line = new Program();
+
+            line.setLength(6.0);
+            Console.WriteLine("Length of line : {0}", line.getLength());
+            Console.ReadKey();
+        }
+    }
+}
