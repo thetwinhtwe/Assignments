c66cd77ba78f4acde9ad84a9b41f0ae98bb4daff
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-02 14:08:38 +0630

    Write Variable and Type Console App

---
+++ VariableType/VariableType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string productName = "Creative";
+            int productYear = 2015;
+            double productPrice = 4.5;
+
+            Console.WriteLine("productName: " + productName);
+            Console.WriteLine("productYear: " + productYear);
+            Console.WriteLine("productPrice: " + productPrice);
+            Console.ReadLine();
+        }
+    }
+}
