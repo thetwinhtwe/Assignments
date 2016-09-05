a925ea2995c32592a6f65a6051cb7127f5e16e25
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-05 12:38:12 +0630

    Write Dictionary Usage Console App

---
+++ DictionaryUsageApp/DictionaryUsageApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryUsageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> inventory = new Dictionary<string, int>();
+            inventory["apple"] = 3;
+            inventory["orange"] = 5;
+            inventory["banana"] = 2;
+
+            Console.WriteLine(inventory["apple"]);
+            Console.WriteLine(inventory["orange"]);
+            Console.WriteLine(inventory["banana"]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DictionaryUsageApp/DictionaryUsageApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DictionaryUsageApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DictionaryUsageApp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fbff2cc2-0733-4c66-be7d-c598e712316d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
