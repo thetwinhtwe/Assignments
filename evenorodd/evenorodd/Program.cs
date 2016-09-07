ab00c9ef8fa2006a2c061093487ac5564e1f9ea3
Thet Win Htwe <thetwinhtwe@gmail.com>
2016-09-07 13:28:03 +0630

    Write Even Or Odd Console App

---
+++ evenorodd/evenorodd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenorodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Enter Your Numbers");
+            num = Convert.ToInt32(Console.ReadLine());
+            String output = (num %2 == 0) ? "Even" : "Odd";
+            Console.WriteLine("You entered: {0}, therefore your number is {1}",num,output);
+            Console.ReadKey();
+        }
+    }
+}
